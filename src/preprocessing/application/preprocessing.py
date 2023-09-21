bf87ed480c74bbc8f38e5f600273d8594c8b3423
Petter Axcell <petteraxcell03@gmail.com>
2023-09-21 19:15:38 +0200

    Files read in preprocessing

---
+++ src/preprocessing/application/preprocessing.py
@@ -0,0 +1,8 @@
+from src.preprocessing.infrastructure.get_data import GetData
+FILENAME = str(os.getenv('FILENAME'))
+KIND_FILE = str(os.getenv('KIND_FILE'))
+DATAFRAME = pd.core.frame.DataFrame
+class Preprocessing():
+    def __init__(self) -> None:
+        self.data: DATAFRAME = GetData(FILENAME).read_data(KIND_FILE)
+    
